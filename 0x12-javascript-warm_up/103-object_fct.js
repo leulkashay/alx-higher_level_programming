afd655893c06f258d0939a8750f6dd2af5dbe753
leulkashay <leulkashay2012@gmail.com>
2023-03-15 17:48:31 -0700

    update

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,12 +4,14 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
+
 myObject.incr = function () {
   this.value++;
 };
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
 console.log(myObject);
-myObject.incr();s
+myObject.incr();
 console.log(myObject);
