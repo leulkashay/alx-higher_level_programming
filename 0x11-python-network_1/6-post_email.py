11bb7e4f721029683318b7c2704365aed07db3b7
leulkashay <leulkashay2012@gmail.com>
2023-03-31 04:41:18 -0700

    network task

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,14 +1,15 @@
-Executable File  13 lines (12 sloc)  361 Bytes
 #!/usr/bin/python3
-"""script that takes in a URL and an email address, sends a POST request
-to the passed URL with the email as a parameter, and finally displays
-the body of the response.
+"""Sends a POST request to a given URL with a given email.
+Usage: ./6-post_email.py <URL> <email>
+  - Displays the body of the response.
 """
-import requests
 import sys
+import requests
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
-    r = requests.post(url, data={'email': email})
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
     print(r.text)
